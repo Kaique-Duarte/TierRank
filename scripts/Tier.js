7900ba69b6126f9f36923d917473caabc171c0c6
kaique <kaiqueduarte251@gmail.com>
2026-03-08 18:25:27 -0300

    Arrumei o bug de quando tinha duas imagems o evento de dragstart não iniciava

--- scripts/Tier.js
+++ scripts/Tier.js
@@ -1,20 +1,27 @@
+
 let titulo = document.getElementById("titulo");
 let tierBlocks = document.querySelectorAll("div.dropitem");
-let dragElement = ''
-let target = document.getElementById("tmTarget");
+let dragElement = "";
+let targets = document.querySelectorAll("div.dropzone");
+
+//aqui é onde o drag and drop acontece, o dragElement é a div que está sendo arrastada, e os targets são as divs onde o elemento pode ser dropado. O evento dragstart é acionado quando o usuário começa a arrastar um elemento, e o evento drop é acionado quando o usuário solta o elemento em um target. O evento dragover é necessário para permitir que o elemento seja dropado no target.
+
 
 tierBlocks.forEach((tierBlock) => {
   tierBlock.addEventListener("dragstart", (e) => {
-     dragElement = e.target;
-
+    dragElement = e.currentTarget;
+    console.log(dragElement);
   });
-  
-  target.addEventListener('dragover', (e) => {
-        e.preventDefault();
-    })
-    target.addEventListener('drop', (e) => {
-        target.appendChild(dragElement)  
-        titulo.innerHTML = "Dropou Aqui !!"
-        
-    })
+});
+
+targets.forEach((target) => {
+  target.addEventListener("dragover", (e) => {
+    e.preventDefault();
+  });
+
+  target.addEventListener("drop", (e) => {
+    target.appendChild(dragElement);
+    titulo.innerHTML = "Dropou Aqui !!";
+    console.log(target);
+  });
 });
