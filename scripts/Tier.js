cd8fbdb7d2e535e8317b26f7461b23f69bc0aff5
kaique <kaiqueduarte251@gmail.com>
2026-03-30 21:20:08 -0300

    Pagina Home

--- scripts/Tier.js
+++ scripts/Tier.js
@@ -1,27 +1,24 @@
-
 let titulo = document.getElementById("titulo");
 let tierBlocks = document.querySelectorAll("div.dropitem");
 let dragElement = "";
+//targets são as divs onde o elemento pode ser dropado.
 let targets = document.querySelectorAll("div.dropzone");
 
-//aqui é onde o drag and drop acontece, o dragElement é a div que está sendo arrastada, e os targets são as divs onde o elemento pode ser dropado. O evento dragstart é acionado quando o usuário começa a arrastar um elemento, e o evento drop é acionado quando o usuário solta o elemento em um target. O evento dragover é necessário para permitir que o elemento seja dropado no target.
-
-
+//O evento dragstart é acionado quando o usuário começa a arrastar um elemento
 tierBlocks.forEach((tierBlock) => {
+  
   tierBlock.addEventListener("dragstart", (e) => {
     dragElement = e.currentTarget;
     console.log(dragElement);
   });
 });
-
+//O evento dragover é necessário para permitir que o elemento seja dropado no target.
 targets.forEach((target) => {
   target.addEventListener("dragover", (e) => {
     e.preventDefault();
   });
-
+  //o evento drop é acionado quando o usuário solta o elemento em um target
   target.addEventListener("drop", (e) => {
     target.appendChild(dragElement);
-    titulo.innerHTML = "Dropou Aqui !!";
-    console.log(target);
   });
 });
